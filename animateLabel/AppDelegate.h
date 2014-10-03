3a4399987cd45de86e574dd64ccde82715f238b6
Rob Mayoff <git@rob.dqd.com>
2014-10-03 14:54:01 -0500

    Initial Commit

---
+++ animateLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animateLabel
+//
+//  Created by Rob Mayoff on 10/3/14.
+//  Copyright (c) 2014 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
